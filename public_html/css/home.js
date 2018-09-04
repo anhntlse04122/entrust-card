d71c3269a396036e82768082516dfda3c333a555
lan anh <lananh2.3.96@gmail.com>
2018-09-04 14:24:43 +0700

    fix ver2

--- public_html/css/home.js
+++ public_html/css/home.js
@@ -3,6 +3,15 @@ $(document).ready(function(){
     $('#myCarousel').carousel({
       interval:false
     });
+    
+    //arrow button
+     $('#arrownext').click(function() {
+       $('#myCarousel').carousel('next');
+ });
+ 
+      $('#arrowprev').click(function() {
+       $('#myCarousel').carousel('prev');
+ });
 
 // scroll slides on mouse scroll 
 $('#myCarousel').bind('mousewheel DOMMouseScroll', function(e){
@@ -20,24 +29,24 @@ $('#myCarousel').bind('mousewheel DOMMouseScroll', function(e){
 
 //scroll slides on swipe for touch enabled devices 
 
- 	$("#myCarousel").on("touchstart", function(event){
- 
-        var yClick = event.originalEvent.touches[0].pageY;
-    	$(this).one("touchmove", function(event){
-
-        var yMove = event.originalEvent.touches[0].pageY;
-        if( Math.floor(yClick - yMove) > 1 ){
-            $(".carousel").carousel('next');
-        }
-        else if( Math.floor(yClick - yMove) < -1 ){
-            $(".carousel").carousel('prev');
-        }
-    });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
-    });
-    
-});
+// 	$("#myCarousel").on("touchstart", function(event){
+// 
+//        var yClick = event.originalEvent.touches[0].pageY;
+//    	$(this).one("touchmove", function(event){
+//
+//        var yMove = event.originalEvent.touches[0].pageY;
+//        if( Math.floor(yClick - yMove) > 1 ){
+//            $(".carousel").carousel('next');
+//        }
+//        else if( Math.floor(yClick - yMove) < -1 ){
+//            $(".carousel").carousel('prev');
+//        }
+//    });
+//    $(".carousel").on("touchend", function(){
+//            $(this).off("touchmove");
+//    });
+//    
+//});
     
  // collapse menu after click  
 $(".navbar-nav li a").click(function(event) {
